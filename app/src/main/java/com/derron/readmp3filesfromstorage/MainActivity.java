81be5f852ce04e8c2825ce43db8a529855a198a9
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2020-08-05 03:16:37 +0100

    Refactor to include more natively supported audio media formats: 3gp,flac, gsm, mid, xmp, rtx, ota, rtttl, mp4, ts,mkv, mp3, m4a, aac, wav, ogg

--- app/src/main/java/com/derron/readmp3filesfromstorage/MainActivity.java
+++ app/src/main/java/com/derron/readmp3filesfromstorage/MainActivity.java
@@ -2,6 +2,7 @@ package com.derron.readmp3filesfromstorage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,20 +10,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public static String SD_PATH = "/sdcard/";
+    public String SD_PATH = "/sdcard/";
     MediaPlayer mediaPlayer = new MediaPlayer();
-    static ArrayList<String> musiclist = new ArrayList<>();
-    static Mp3Filter filter = new Mp3Filter();
+    static ArrayList<String> musicList = new ArrayList<>();
     ListView listView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listview);
@@ -30,14 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick (AdapterView<?> adapterView, View view, int i, long l) {
                 try {
                     mediaPlayer.reset();
-                    mediaPlayer.setDataSource(SD_PATH + musiclist.get(i));
+                    mediaPlayer.setDataSource(SD_PATH + musicList.get(i));
                     mediaPlayer.prepare();
                     mediaPlayer.start();
                 } catch (IOException e) {
-                    Log.v(getString(R.string.app_name),e.getMessage());
+                    Log.v(getString(R.string.app_name), e.getMessage());
                 }
 
             }
@@ -45,15 +47,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void updateList(){
-        File dir = new File(SD_PATH);
-        if (dir.listFiles(filter).length > 0){
-            for (File file: dir.listFiles(filter)){
-                musiclist.add(file.getName());
-            }
-        }
+    private void updateList () {
+        File rootDir = new File(SD_PATH);
+        if (rootDir.isDirectory()) {
+            try {
+                File[] files = rootDir.listFiles(new FilenameFilter() {
+                    @Override
+                    public boolean accept (File file, String name) {
+                        if (name.lastIndexOf('.') > 0 ){
+                            String extension = name.substring(name.lastIndexOf('.')+1).toLowerCase();
+                            Toast formats = Toast.makeText(MainActivity.this.getApplicationContext(), "Format " + extension + " found", Toast.LENGTH_SHORT);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.songs,musiclist);
+                            switch (extension) {
+                                case "mp3":
+                                case "aac":
+                                case "flac":
+                                case "gsm":
+                                case "m4a":
+                                case "mid":
+                                case "ogg":
+                                case "ota":
+                                case "rtttl":
+                                case "rtx":
+                                case "xmp":
+                                case "wav":
+                                    if (formats != null) {
+                                        formats.cancel();
+                                    }
+                                    formats.show();
+                                    return true;
+                                default:
+                                    return false;
+                            }
+                        }
+                        return false;
+                    }
+                });
+                if (files != null && files.length > 0) {
+                    for (File file : files) {
+                        musicList.add(file.getName());
+                    }
+                } else
+                    Toast.makeText(this, "File(s) couldn't be found.", Toast.LENGTH_LONG).show();
+            } catch (NullPointerException npe) {
+                npe.getLocalizedMessage();
+            }
+        } else
+            Toast.makeText(this, "Specified/Hard-coded path is not a directory!", Toast.LENGTH_LONG).show();
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.songs, musicList);
         listView.setAdapter(adapter);
     }
 
